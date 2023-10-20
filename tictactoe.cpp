80d4b33374eaba395af5a03bc22c737401d3fdbf
MastayY <93984625+MastayY@users.noreply.github.com>
2023-10-20 19:22:53 +0700

    Update tictactoe.cpp

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -4,175 +4,98 @@
 using namespace std;
 
 int main() {
-    // papan permainan
-    string row_1[3] = {" ", " ", " "};
-    string row_2[3] = {" ", " ", " "};
-    string row_3[3] = {" ", " ", " "};
+    string board[9] = {" ", " ", " ", " ", " ", " ", " ", " ",  " "};
 
-    bool win = false;
-    char turn = '1';
+    int win = -1;
+    int turn = 1;
 
-    // logic
-
-    while(!win) {
-        int p1RowChoice;
-        int p1ColumnChoice;
-        int p2RowChoice;
-        int p2ColumnChoice;
+    while(win == -1) {
+        int choice;
 
         cout << "=====================================" << endl;
-        cout << "            TicTacToe Game           " << endl;
+        cout << "           TicTacToe Games           " << endl;
         cout << "=====================================" << endl;
-        cout << "Dibuat oleh Mastay\n\nSocial Media\nGithub : https://github.com/MastayY\nIG : https://instagram.com/nniceone._" << endl;
-        cout << "\nKeterangan :\nBaris : Mendatar\nKolom : Menurun\nPlayer 1 : 'X'\nPlayer 2 : 'O'" << endl;
+        cout << "Dibuat oleh Mastay\nSocial Media :\nIG : https://instagram.com/nniceone._\nGithub : https://github.com/MastayY\n" << endl;
+        cout << "KETERANGAN :\nPlayer 1 : 'X'\nPlayer 2 : 'O'" << endl;
         cout << "=====================================\n" << endl;
 
-
-        cout << "\t\t" << row_1[0] << "|" << row_1[1] << "|" << row_1[2] << endl;
-        cout << "\t\t" << "-" << " - " << "-" << endl;
-        cout << "\t\t" << row_2[0] << "|" << row_2[1] << "|" << row_2[2] << endl;
-        cout << "\t\t" << "-" << " - " << "-" << endl;
-        cout << "\t\t" << row_3[0] << "|" << row_3[1] << "|" << row_3[2] << endl;
+        cout << "\t     |     |     " << endl;
+        cout << "\t  " << board[0] << "  |  " << board[1] << "  |  " << board[2] << endl;
+        cout << "\t_____|_____|_____" << endl;
+        cout << "\t     |     |     " << endl;
+        cout << "\t  " << board[3] << "  |  " << board[4] << "  |  " << board[5] << endl;
+        cout << "\t_____|_____|_____" << endl;
+        cout << "\t     |     |     " << endl;
+        cout << "\t  " << board[6] << "  |  " << board[7] << "  |  " << board[8] << endl;
+        cout << "\t     |     |     " << endl << endl;
 
         cout << "\n=====================================" << endl;
 
-        if((row_1[0] == "x" && row_1[1] == "x" && row_1[2] == "x") || (row_2[0] == "x" && row_2[1] == "x" && row_2[2] == "x") || (row_3[0] == "x" && row_3[1] == "x" && row_3[2] == "x") || (row_1[0] == "x" && row_2[0] == "x" && row_3[0] == "x") ||(row_1[1] == "x" && row_2[1] == "x" && row_3[1] == "x") || (row_1[2] == "x" && row_2[2] == "x" && row_3[2] == "x") || (row_1[0] == "x" && row_2[1] == "x" && row_3[2] == "x") || (row_3[0] == "x" && row_2[1] == "x" && row_1[2] == "x")) {
+        if((board[0] == "X" && board[0] == board[1] && board[1] == board[2]) || ( board[3] == "X" &&board[3] == board[4] && board[4] == board[5]) || (board[6] == "X" && board[6] == board[7] && board[7] == board[8]) || (board[0] == "X" && board[0] == board[3] && board[3] == board[6]) || (board[1] == "X" && board[1] == board[4] && board[4] == board[7]) || (board[2] == "X" && board[2] == board[5] && board[5] == board[8]) || (board[0] == "X" && board[0] == board[4] && board[4] == board[8]) || (board[2] == "X" && board[2] == board[4] && board[4] == board[6])) {
             cout << "\nPlayer 1 Menang\n" << endl;
-            win = true;
             break;
-        } else if((row_1[0] == "o" && row_1[1] == "o" && row_1[2] == "o") || (row_2[0] == "o" && row_2[1] == "o" && row_2[2] == "o") || (row_3[0] == "o" && row_3[1] == "o" && row_3[2] == "o") || (row_1[0] == "o" && row_2[0] == "o" && row_3[0] == "o") ||(row_1[1] == "o" && row_2[1] == "o" && row_3[1] == "o") || (row_1[2] == "o" && row_2[2] == "o" && row_3[2] == "o") || (row_1[0] == "o" && row_2[1] == "o" && row_3[2] == "o") || (row_3[0] == "o" && row_2[1] == "o" && row_1[2] == "o")) {
+        } else if((board[0] == "O" && board[0] == board[1] && board[1] == board[2]) || ( board[3] == "O" &&board[3] == board[4] && board[4] == board[5]) || (board[6] == "O" && board[6] == board[7] && board[7] == board[8]) || (board[0] == "O" && board[0] == board[3] && board[3] == board[6]) || (board[1] == "O" && board[1] == board[4] && board[4] == board[7]) || (board[2] == "O" && board[2] == board[5] && board[5] == board[8]) || (board[0] == "O" && board[0] == board[4] && board[4] == board[8]) || (board[2] == "O" && board[2] == board[4] && board[4] == board[6])) {
             cout << "\nPlayer 2 Menang\n" << endl;
-            win = true;
             break;
         }
 
-        cout << "\nGiliran : Player " << turn << endl;
+        cout << "Player " << turn << " masukkan nomor index papan : ";
+        cin >> choice;
 
-        if(turn == '1') {
-            cout << "\nBaris Pilihan anda ? ";
-            cin >> p1RowChoice;
-
-            if(p1RowChoice - 1 > 2) {
-                system("cls");
-                cout << "\n[ERROR] Baris tidak ada. Baris yang ada : 1, 2, 3\n" << endl;
-            } else if(p1RowChoice == 1 && (row_1[0] == " " || row_1[1] == " " || row_1[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p1ColumnChoice;
-
-                if(p1ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_1[p1ColumnChoice - 1] != " ") {
+        if(turn == 1) {
+            switch(choice) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    if(board[choice - 1] != "X" && board[choice - 1] != "O") {
+                        board[choice - 1] = "X";
+                        turn = 2;
                         system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
                     } else {
-                        row_1[p1ColumnChoice - 1] = 'x';
-                        turn = '2';
                         system("cls");
+                        cout << "\n[ERROR] Pilihan sudah diisi\n" << endl;
                     }
-                }
-            } else if(p1RowChoice == 2 && (row_2[0] == " " || row_2[1] == " " || row_2[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p1ColumnChoice;
 
-                if(p1ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_2[p1ColumnChoice - 1] != " ") {
-                        system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
-                    } else {
-                        row_2[p1ColumnChoice - 1] = 'x';
-                        turn = '2';
-                        system("cls");
-                    }
-                }
-            } else if(p1RowChoice == 3 && (row_3[0] == " " || row_3[1] == " " || row_3[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p1ColumnChoice;
+                    break;
 
-                if(p1ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_3[p1ColumnChoice - 1] != " ") {
-                        system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
-                    } else {
-                        row_3[p1ColumnChoice - 1] = 'x';
-                        turn = '2';
-                        system("cls");
-                    }
-                }
-            } else {
-                system("cls");
-                cout << "Baris sudah terisi semua\n" << endl;
+                default:
+                    cout << "\n[ERROR] Pilih papan dari 1-9\n" << endl;
+                    break;
             }
-
         } else {
-            cout << "\nBaris Pilihan anda ? ";
-            cin >> p2RowChoice;
-
-            if(p2RowChoice - 1 > 2) {
-                system("cls");
-                cout << "\n[ERROR] Baris tidak ada. Baris yang ada : 1, 2, 3\n" << endl;
-            } else if(p2RowChoice == 1 && (row_1[0] == " " || row_1[1] == " " || row_1[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p2ColumnChoice;
-
-                if(p2ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_1[p2ColumnChoice - 1] != " ") {
+            switch(choice) {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    if(board[choice - 1] != "X" && board[choice - 1] != "O") {
+                        board[choice - 1] = "O";
+                        turn = 1;
                         system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
                     } else {
-                        row_1[p2ColumnChoice - 1] = 'o';
-                        turn = '1';
                         system("cls");
+                        cout << "\n[ERROR] Pilihan sudah diisi\n" << endl;
                     }
-                }
-            } else if(p2RowChoice == 2 && (row_2[0] == " " || row_2[1] == " " || row_2[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p2ColumnChoice;
 
-                if(p2ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_2[p2ColumnChoice - 1] != " ") {
-                        system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
-                    } else {
-                        row_2[p2ColumnChoice - 1] = 'o';
-                        turn = '1';
-                        system("cls");
-                    }
-                }
-            } else if(p2RowChoice == 3 && (row_3[0] == " " || row_3[1] == " " || row_3[2] == " ")){
-                cout << "\nKolom Pilihan anda ? ";
-                cin >> p2ColumnChoice;
+                    break;
 
-                if(p2ColumnChoice - 1 > 2) {
-                    system("cls");
-                    cout << "\n[ERROR] Kolom tidak ada. Kolom yang ada : 1, 2, 3\n" << endl;
-                } else {
-                    if(row_3[p2ColumnChoice - 1] != " ") {
-                        system("cls");
-                        cout << "[ERROR] Kolom sudah terisi\n" << endl;
-                    } else {
-                        row_3[p2ColumnChoice - 1] = 'o';
-                        turn = '1';
-                        system("cls");
-                    }
-                }
-            } else {
-                system("cls");
-                cout << "Baris sudah terisi semua\n" << endl;
+                default:
+                    cout << "\n[ERROR] Pilih papan dari 1-9\n" << endl;
+                    break;
             }
         }
+
+
     }
 }
